26d009f53e1b359966c80b6665bf8224702bd553
Noer Paanakker <noer@lightworksweb.nl>
2022-07-17 11:37:52 +0200

    fetch posts SSR

---
+++ pages/api/posts/index.ts
@@ -0,0 +1,16 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { allPostsQuery } from '../../../utils/queries';
+import { client } from '../../../utils/sanity-client';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  switch (req.method) {
+    case 'GET':
+      const query = allPostsQuery();
+      const data = await client.fetch(query);
+      console.log('data is', data);
+
+      return res.status(200).json(data);
+    default:
+      return res.status(404).json('It no work...');
+  }
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,60 @@
+import axios, { AxiosResponse } from 'axios';
 import type { NextPage } from 'next';
 
-const Home: NextPage = () => {
+interface IPostsResponse {
+  caption: string;
+  video: {
+    asset: {
+      _id: string;
+      url: string;
+    };
+  };
+  _id: string;
+  postedBy: {
+    _id: string;
+    userName: string;
+    image: string;
+  };
+  likes: {
+    postedBy: {
+      _id: string;
+      userName: string;
+      image: string;
+    };
+  }[];
+  comments: {
+    comment: string;
+    _key: string;
+    postedBy: {
+      _ref: string;
+    };
+  }[];
+  userId: string;
+}
+
+interface IHomeProps {
+  posts: IPostsResponse;
+}
+
+const Home: NextPage<IHomeProps> = ({ posts }) => {
+  console.log('posts is', posts);
   return <h1 className="text-3xl font-bold underline">TikTok Clone</h1>;
+};
+
+export const getServerSideProps = async () => {
+  const BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || 'http://localhost:3000/api';
+
+  try {
+    const response: AxiosResponse<IPostsResponse[], null> = await axios.get(`${BASE_URL}/posts`);
+
+    console.log('response', response);
+
+    return {
+      props: { posts: response.data },
+    };
+  } catch (error) {
+    return 'Error!';
+  }
 };
 
 export default Home;
